81fe82ee5a880094a80e3c19109eb428ce3d6250
StormESky <79471824+StormESky@users.noreply.github.com>
2021-06-13 20:37:41 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -13,7 +13,7 @@ function startGame() {
 var myGameArea = {
     canvas : document.createElement("canvas"),
     start : function() {
-        this.canvas.width = 500;
+        this.canvas.width = 350;
         this.canvas.height = 500;
         this.context = this.canvas.getContext("2d");
         document.body.insertBefore(this.canvas, document.body.childNodes[0]);
